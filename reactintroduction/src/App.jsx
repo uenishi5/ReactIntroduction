8bdc71790e840788be15ba08cc6100f6cbbc29d4
masayuki <hcsabc2019@gmail.com>
2024-01-22 23:45:05 +0900

    StyledJSX

--- reactintroduction/src/App.jsx
+++ reactintroduction/src/App.jsx
@@ -3,12 +3,14 @@ import { useState, useCallback } from "react";
 import { ChildArea } from "./component/childArea";
 import { CssModules } from "./component/CssModules";
 import { InlineStyles } from "./component/InlineStyles";
+import { StyledJSX } from "./component/StyledJsx";
 
 export const App = () => {
   return (
     <>
       <InlineStyles></InlineStyles>
       <CssModules></CssModules>
+      <StyledJSX></StyledJSX>
     </>
   );
 };
